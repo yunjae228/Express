1926a2e1760877d790600741e5e479549a52895b
yunjae228 <yvkqzhs7@gmail.com>
2021-12-09 19:30:04 +0900

    Rest API Routing

--- src/main.js
+++ src/main.js
@@ -1,31 +1,56 @@
 // @ts-check
 
 const express = require('express')
-const fs = require('fs')
+
+const userRouter = express.Router()
+
+const bodyParser = require('body-parser')
+
 const app = express()
+app.use(bodyParser.json())
 
 const PORT = 5000
 
-app.use('/', async (req, res, next) => {
-  console.log('middleware 1-1')
+userRouter.get('/', (req, res) => {
+  res.send('User list')
+})
 
-  const fileContent = await fs.promises.readFile('.gitignore')
+const USERS = {
+  3: {
+    nickname: 'foo',
+  },
+}
 
-  const requestedAt = new Date()
+userRouter.param('id', (req, res, next, value) => {
+  console.log(`id parameter ${value}`)
   //@ts-ignore
-  req.requestedAt = requestedAt
-  //@ts-ignore
-  req.fileContent = fileContent
+  req.user = USERS[value]
   next()
 })
 
-app.use((req, res) => {
-  console.log('Middleware 2')
-  res.send(
-    //@ts-ignore
-    `hello, express: Requested at  ${req.requestedAt}, ${req.fileContent}`
-  )
+//:id는 path parameter
+userRouter.get('/:id', (req, res) => {
+  console.log('userRouter get ID')
+  //@ts-ignore
+  res.send(req.user)
 })
+
+userRouter.post('/', (req, res) => {
+  //Register user
+  res.send('User registered')
+})
+
+userRouter.post('/:id/nickname', (req, res) => {
+  //@ts-ignore
+  const { user } = req
+  const { nickname } = req.body
+
+  user.nickname = nickname
+
+  res.send(`User nickname updated: ${nickname}`)
+})
+
+app.use('/users', userRouter)
 
 app.listen(PORT, () => {
   console.log('The express server is listening at port:', PORT)
